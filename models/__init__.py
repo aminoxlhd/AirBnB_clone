7c7b3781b68a82a54f97d0597bac852061743acf
root <aminoxlhd>
2023-12-06 13:29:04 -0800

    add base_model.py __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""" instantiates the storage system """
+
+
+from models.base_model import BaseModel
---
+++ models/base_model.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from uuid import uuid4
+from datetime import datetime
+""" BaseModel class """
+
+
+class BaseModel:
+    """ defines all common attributes/methods for other classes """
+
+    def __init__(self):
+        self.id = uuid4()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dict = self.__dict__.copy()
+        dict['created_at'] = self.created_at.isoformat()
+        dict['updated_at'] = self.updated_at.isoformat()
+        dict['__class__'] = self.__class__.__name__
+        return dict
