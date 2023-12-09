6a40091b15eef8fe073b17d150c6471c711cd088
root <aminoxlhd>
2023-12-09 11:28:12 -0800

    edit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,31 +1,45 @@
 #!/usr/bin/python3
 """ FileStorage class """
+
 import json
+
 
 class FileStorage:
     """ class FileStorage """
-    def _init_(self):
+    def __init__(self):
         self.__file_path = "file.json"
         self.__objects = {}
+
 
     def all(self):
         return self.__objects
 
+
     def new(self, obj):
-        key = obj._class.name_ + "." + obj.id
-        self.__objects[key] = obj
+        key = obj.__class__.__name__ + "." + obj.id
+        self.__objects[key] = obj.to_dict()
+
 
     def save(self):
-        with open(self.__file_path, "w") as json_file:
-            json.dump(temp, json_file)
+        list_objects = {}
+        for key, value in self.__objects.items():
+            list_objects[key] = value
+        with open(self.__file_path, 'w') as file:
+            json.dump(list_objects, file)
 
     def reload(self):
-        try:
-            with open(self.__file_path, "r") as json_file:
-                dict = json.load(json_file)
-                for key, dict in dict.items():
-                    dict_instance = models.dummy_classes[dict["__class__"]](**dict)
-                    self.__objects[key] = dict_instance
+        with open(self.__file_path, 'r') as file:
+            objects_json = json.loads(file.read())
+            for obj in objects_json.values():
+                self.__objects[f"{obj['__class__']}.{obj['id']}"] = obj
 
-        except:
-            pass
+
+    def destroy(self, obj):
+        obj_key = f"{obj['__class__']}.{obj['id']}"
+        del self.__objects[obj_key]
+
+    def update(self, obj, attribute_key, attribute_value):
+        obj_key = f"{obj['__class__']}.{obj['id']}"
+        obj[attribute_key] = attribute_value
+        self.__objects[obj_key] = obj
+
