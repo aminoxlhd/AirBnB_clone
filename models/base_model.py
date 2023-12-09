ca4851460f022fa59644b786683d0f6e3b25b523
root <aminoxlhd>
2023-12-09 04:10:06 -0800

    edit base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
 from uuid import uuid4
 from datetime import datetime
-""" BaseModel class """
+from models import storage
+
+""" BaseModel class"""
 
 
 class BaseModel:
     """ defines all common attributes/methods for other classes """
 
-    def __init__(self):
-        self.id = uuid4()
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, **wkargs):
+        if wkargs:
+            dict = wkargs.copy()
+            dict['created_at'] = datetime.strptime(dict['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            dict['updated_at'] = datetime.strptime(dict['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            self._dict_ = dict
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         dict = self.__dict__.copy()
