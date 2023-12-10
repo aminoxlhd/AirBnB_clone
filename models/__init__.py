8383ed711aa1b43a66e4fe952cf60df284a82505
root <aminoxlhd>
2023-12-10 08:25:31 -0800

    edit __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,19 @@
 
 
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
+from models.user import User
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.state import State
+from models.place import Place
 
+from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
+
+my_models = ["BaseModel", 
+"User", "Amenity", 
+"City", "Place",
+ "Review", "State",
+ "User"]
